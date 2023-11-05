2adb2c9a3a36575bec2ee32b1f22e35d52006157
nogi.triwardana <nogi.triwardana@smt-id.com>
2023-11-05 10:48:14 +0700

    initial create project app

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+import { useLayoutEffect, useEffect } from "react";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import MealsList from "../components/MealList/MealList";
+
+function MealsOverviewScreen({ route, navigation }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+
+    navigation.setOptions({
+      title: categoryTitle
+    });
+  },[catId, navigation]);
+
+  return <MealsList items={displayedMeals} />
+}
+
+export default MealsOverviewScreen;
